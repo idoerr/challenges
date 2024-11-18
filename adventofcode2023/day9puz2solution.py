2e753a0665ab7d0b1d4e5e9a3d5d61d00143df03
Ian Doerr <ian.mk.doerr@gmail.com>
2024-11-18 15:51:35 -0500

    Complete day 9 puzzles.

---
+++ adventofcode2023/day9puz2solution.py
@@ -0,0 +1,32 @@
+
+def get_previous_number_recurse(num_arr):
+
+    delta_arr = []
+
+    for current, next in zip(num_arr, num_arr[1:]):
+        delta_arr.append(next - current)
+    
+    if set(delta_arr) == {0}:
+        return num_arr[0]
+    
+    recurse_delta = get_previous_number_recurse(delta_arr)
+
+    return num_arr[0] - recurse_delta
+
+
+if __name__ == "__main__":
+    file_name = 'adventofcode2023/day9puz1input.txt'
+
+    next_numbers = []
+
+    with open(file_name, 'r') as file_handle:
+        for line in file_handle:
+
+            number_arr = list(map(int, line.split(' ')))
+
+            next_number = get_previous_number_recurse(number_arr)
+
+            next_numbers.append(next_number)
+
+    print('k')
+    print(sum(next_numbers))
